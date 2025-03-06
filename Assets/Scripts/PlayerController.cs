e8551727fa72bd334e78d01fb34172d525b74245
ManJin12 <hh5570hh@naver.com>
2025-03-06 23:17:57 +0900

    [Add]: 캐릭터 애니메이션 적용

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -23,15 +24,17 @@ public class PlayerController : MonoBehaviour
 
     private Vector2 mouseDelta;
     private Rigidbody rigid;
-    //private Animator anim;
+    private Animator anim;
 
-    private bool isJumping = false; // 점프 중인지 체크
+    public bool isJumping = false; // 점프 중인지 체크
+    private float lastGroundTime;
+    private float jumpDelay = 0.1f;
 
 
     private void Awake()
     {
         rigid = GetComponent<Rigidbody>();
-        //anim = GetComponentInChildren<Animator>();
+        anim = GetComponentInChildren<Animator>();
     }
 
     private void FixedUpdate()
@@ -42,6 +45,8 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
+        DebugGroundCheck();
+        CheckJumpState();
 
     }
     private void LateUpdate()
@@ -69,10 +74,12 @@ public class PlayerController : MonoBehaviour
         if (context.phase == InputActionPhase.Performed)
         {
             curMovementInput = context.ReadValue<Vector2>();
+            anim.SetBool("IsWalk", true);
         }
         else if(context.phase == InputActionPhase.Canceled)
         {
             curMovementInput = Vector2.zero;
+            anim.SetBool("IsWalk", false);
         }
        
     }
@@ -91,11 +98,16 @@ public class PlayerController : MonoBehaviour
         if(context.phase == InputActionPhase.Started && IsGrounded())
         {
             rigid.AddForce(Vector2.up * jumpPower, ForceMode.Impulse);
+            anim.SetTrigger("IsJump");
+            isJumping = true;
+            lastGroundTime = Time.time;
         }
     }
     
     private bool IsGrounded()
     {
+        if (Time.time - lastGroundTime < jumpDelay) return false; // 점프 후 일정 시간 동안 착지 판정 X
+
         Ray[] rays = new Ray[4]
         {
             new Ray(transform.position + (transform.forward * 0.2f) + (transform.up * 0.01f), Vector3.down),
@@ -104,15 +116,55 @@ public class PlayerController : MonoBehaviour
             new Ray(transform.position + (-transform.right * 0.2f) + (transform.up * 0.01f), Vector3.down)
         };
 
+        float rayLength = 0.3f;
+
         for (int i = 0; i < rays.Length; i++)
         {
-            if (Physics.Raycast(rays[i], 0.3f, groundLayerMask))
+            if (Physics.Raycast(rays[i], rayLength, groundLayerMask))
             {
+                lastGroundTime = Time.time; // 땅에 닿은 시간 업데이트
                 return true;
             }
         }
 
         return false;
+    }
 
+    void DebugGroundCheck()
+    {
+        // Ray를 씬 뷰에서 보이게 하기 위한 길이
+        float rayLength = 0.3f; // 기존보다 길이 늘림
+        Color rayColor = Color.green; // 보기 편하게 초록색 지정
+
+        // 4방향으로 Ray 설정
+        Vector3[] rayOrigins = new Vector3[4]
+        {
+        transform.position + (transform.forward * 0.2f) + (transform.up * 0.01f),
+        transform.position + (-transform.forward * 0.2f) + (transform.up * 0.01f),
+        transform.position + (transform.right * 0.2f) + (transform.up * 0.01f),
+        transform.position + (-transform.right * 0.2f) + (transform.up * 0.01f)
+        };
+
+        // Ray를 4방향으로 그리기
+        for (int i = 0; i < rayOrigins.Length; i++)
+        {
+            Debug.DrawRay(rayOrigins[i], Vector3.down * rayLength, rayColor, 0.1f);
+        }
+    }
+
+    public void CheckJumpState()
+    {
+        if (isJumping)
+        {
+            if (rigid.velocity.y > 0.1f) // 상승 중일 때 Jump_loop 실행
+            {
+                anim.SetBool("IsJumping", true);
+            }
+            else if (rigid.velocity.y <= 0f && Time.time - lastGroundTime >= jumpDelay) // 착지 감지
+            {
+                anim.SetBool("IsJumping", false);
+                isJumping = false; // 점프 상태 해제
+            }
+        }
     }
 }
