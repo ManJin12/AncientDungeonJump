0fc16075729cf7c99a63fbfce684d2dcbc831e78
ManJin12 <hh5570hh@naver.com>
2025-03-06 14:30:07 +0900

    [Add]: 캐릭터, 배경 에셋 추가

---
+++ Assets/ElfArden/Scripts/JumpSetting.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class JumpSetting : MonoBehaviour
+{
+    private enum JumpState
+    {
+        Weapon,
+        NoWeapon
+    }
+
+    [SerializeField] private JumpState state;
+    [SerializeField] private float timePreCast;
+    [SerializeField] private float timeCast;
+    [SerializeField] private float timePostCast;
+    [SerializeField] private float percentPreCast;
+    [SerializeField] private float percentCast;
+
+    public float TimePreCast => timePreCast;
+    public float TimeCast => timeCast;
+    public float TimePostCast => timePostCast;
+    public float PercentPreCast => percentPreCast;
+    public float PercentCast => percentCast;
+}
